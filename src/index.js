8c5b36c129ffe350da954db03b72f4354e5245e7
Viktar Svintsitski <jetalai3@gmail.com>
2019-11-06 01:46:50 +0300

    feat: add files required for hosting

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './assets/css/reset.css';
-import './assets/css/styles.css';
-
-import App from './application/components/app';
-
-window.addEventListener('DOMContentLoaded', () => {
-  const app = new App();
-  app.start();
-});
